cae47dd955dd40e9ec921eb835ad519195372df2
toshiomi <19881005mnmtsom@gmail.com>
2019-10-14 13:03:43 +0900

    三項演算子の使用

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){ 
   function buildHTML(message){
-   if ( message.image ) {
+     var new_image = message.image ? `<img class= "lower-message__image" src= ${message.image} >` : "";
      var html =
       `<div class="message" data-message-id=${message.id}>
          <div class="upper-message">
@@ -15,30 +15,10 @@ $(function(){
            <p class="lower-message__content">
              ${message.content}
            </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
+           ${new_image}
+         </div>`
      return html;
-   } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
+  };
 $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
