5177e6f54ac99ce3f74f6bf58e07d9f27cae30c5
toshiomi <19881005mnmtsom@gmail.com>
2019-10-16 21:30:33 +0900

    事前修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -45,27 +45,19 @@ $('#new_message').on('submit', function(e){
  })
 
   var reloadMessages = function() {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     var last_message_id = $('.message:last').attr("data-id");
-    var url = "/groups/18/api/messages"
+    var group_id = $(".messages").attr("id");
+    var url = "/groups/"+ group_id +"/api/messages"
     $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: url,
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      //追加するHTMLの入れ物を作る
       var insertHTML = '';
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         messages.forEach(function (message){
-      //メッセージが入ったHTMLを取得
           insertHTML = buildHTML(message);
-      //メッセージを追加
           $('.messages').append(insertHTML);
       })
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
@@ -73,7 +65,8 @@ $('#new_message').on('submit', function(e){
     .fail(function() {
       console.log('バーカ');
     });
-  }
   };
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){
   setInterval(reloadMessages, 5000);
+  }
 });
