f739303f7a501d9915fcd7edfe1a86329ed3c001
toshiomi <19881005mnmtsom@gmail.com>
2019-10-16 20:46:58 +0900

    自動更新の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function(){ 
   function buildHTML(message){
-     var new_image = message.image ? `<img class= "lower-message__image" src= ${message.image} >` : "";
+     var new_image = message.image_url ? `<img class= "lower-message__image" src= ${message.image_url} >` : "";
      var html =
       `<div class="message" data-message-id=${message.id}>
          <div class="upper-message">
@@ -18,7 +18,8 @@ $(document).on('turbolinks:load', function(){
            ${new_image}
          </div>`
      return html;
-  };
+  }
+
 $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
@@ -34,12 +35,45 @@ $('#new_message').on('submit', function(e){
   .done(function(data){
     var html = buildHTML(data);
     $('.messages').append(html);
-    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
+    $('.messages').animate({scrollTop: $('.messages')[0].scroll}, 'fast');   
     $('form')[0].reset();
   })
    .fail(function(){
      alert('error');
    });
    return false;
- });
+ })
+
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message:last').attr("data-id");
+    var url = "/groups/18/api/messages"
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: url,
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        messages.forEach(function (message){
+      //メッセージが入ったHTMLを取得
+          insertHTML = buildHTML(message);
+      //メッセージを追加
+          $('.messages').append(insertHTML);
+      })
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    }) 
+    .fail(function() {
+      console.log('バーカ');
+    });
+  }
+  };
+  setInterval(reloadMessages, 5000);
 });
